5c8fc201d20c8e879a06286739db652d9cfa8a2c
LeonardoRorizBessa <leonardo.rorizbessa@gmail.com>
2025-04-29 09:15:39 -0300

    feat: header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,43 @@
 import { NavLink, Link } from "react-router"
+import logo from "../assets/logo.png"
 
 const Header = () => {
   return ( 
     <>
-      <div className="flex justify-between px-[135px] py-6 bg-gray-700 text-base text-white font-bold">
-        <nav className="flex gap-8">
-          <h1>LOGO</h1>
+      <div className="flex justify-between px-[135px] py-[30px] bg-preto text-base text-branco">
+        <nav className="flex justify-center items-center gap-[45px] font-[500]">
+          <img 
+            className="cursor-pointer"
+            src={logo} 
+            alt="Logo Tiamate" 
+          />
           <NavLink 
             to={"/"}
-            className="hover:text-red-500 transition duration-300"
+            className="hover:text-marron-claro transition duration-300"
           >
             Inicio
           </NavLink>
           <NavLink 
             to={"/nosso-cafe"}
-            className="hover:text-red-500 transition duration-300"
+            className="hover:text-marron-claro transition duration-300"
           >
             Nosso Café
           </NavLink>
           <NavLink 
             to={"/noticias"}
-            className="hover:text-red-500 transition duration-300"
+            className="hover:text-marron-claro transition duration-300"
           >
             Notícias
           </NavLink>
           <NavLink 
             to={"/contato"}
-            className="hover:text-red-500 transition duration-300"
+            className="hover:text-marron-claro transition duration-300"
           >
             Contato
           </NavLink>
           <NavLink 
             to={"/cardapio"}
-            className="hover:text-red-500 transition duration-300"
+            className="hover:text-marron-claro transition duration-300"
           >
             Cardápio
           </NavLink>
@@ -40,7 +45,7 @@ const Header = () => {
         <button>
           <Link
             to={"/franquia"}
-            className="p-3 rounded-md bg-orange-500 text-black"
+            className="px-[23px] py-[16px] rounded-[5px] bg-marron-claro text-black text-base hover:bg-vinho hover:text-marron-claro transition duration-300"
           >
             Seja um Franquiado
           </Link>
